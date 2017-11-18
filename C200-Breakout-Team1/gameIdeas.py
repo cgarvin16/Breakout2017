453e8ba23279808ae0945e647f50b4d20a8cdb6b
Carolyn Garvin <ctgarvin@iu.edu>
2017-11-18 00:21:14 -0500

    notes

---
+++ C200-Breakout-Team1/gameIdeas.py
@@ -0,0 +1,16 @@
+#In the class: it has a point value
+#if the ball hits the brick it dissapears and take that value of the brick to the total value
+#can draw bricks from the sprite. which uses coordinates of a huge picture
+#make a graphic when the ball hits the brick it explodes and maybe a sound
+#give it a certain amount of lives 
+
+#themed and themed background = christmas
+#sticky paddle where ball sticks to the paddle
+#specialty bricks, one that speeds up the ball, one that slows it down, one that releases extra balls, one that shrink/grows paddle
+#brick that give you extra lives, you have to catch the falling life
+#as levels go up the bricks need more than one hit to dissapear
+#have to make files that saves the gamers scores so not everythng is lost when game is turned off
+#instructions screne, a "how to"
+#a you win or you lose game, would u like to play again or quit?
+
+#main class game (attribute to keep score and life count), subclass' menu (theme selection, difficulty selection) wall (subclass brick (attributes special bricks/powerups)) paddle (method to grow/shrink) & ball
