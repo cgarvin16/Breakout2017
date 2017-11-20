06e9ab6059703ede5cf7fd7a18d76431d21c30c4
Carolyn Garvin <ctgarvin@iu.edu>
2017-11-19 23:28:23 -0500

    bricks

--- C200-Breakout-Team1/Brick.py
+++ C200-Breakout-Team1/Brick.py
@@ -1,5 +1,24 @@
 #a file to contain our brick class -LF
 import pygame as py
+
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
 class Brick:
     def __init__(self):
         pass
+#drawing the bricks and coordinates
+    py.draw.rect(gameDisplay, red, [0,0,100,30])
+    py.draw.rect(gameDisplay, red, [100,0,100,30])
+    py.draw.rect(gameDisplay, red, [200,0,100,30])
+    py.draw.rect(gameDisplay, red, [300,0,100,30])
+    py.draw.rect(gameDisplay, red, [400,0,100,30])
+    py.draw.rect(gameDisplay, red, [500,0,100,30])
+    py.draw.rect(gameDisplay, red, [600,0,100,30])
+    py.draw.rect(gameDisplay, green, [0,100,100,30])
+
+    
+
+    py.display.update()
