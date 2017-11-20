7e59a11c993d8ba423a8b0bb48b17bfbbbe5c6a8
Charles Brooks <you@example.com>
2017-11-20 12:10:25 -0500

    added some paddle code

--- C200-Breakout-Team1/Paddle.py
+++ C200-Breakout-Team1/Paddle.py
@@ -6,4 +6,5 @@ from Ball import Ball
 #beginning of the class used to create the paddle object
 class Paddle:
     def __init__(self):
+        self.image = py.image.load(C:/pthon/paddleImage.png)
         pass
