8ffb23ec05448f00c80ce42804ca1fac511db94b
lofields <lofields@umail.iu.edu>
2017-11-19 20:10:13 -0500

    added non-random movement to ball

--- C200-Breakout-Team1/gameDisplay.py
+++ C200-Breakout-Team1/gameDisplay.py
@@ -6,12 +6,13 @@ from Brick import Brick
 from Paddle import Paddle 
 import pygame as py
 import sys
-
+import random
 #beginning of the required pygame skeleton
 py.init()
 
 #sets the game display size as 800px width and 700px height
 gameDisplay = py.display.set_mode((800, 700))
+
 #adds title to window top of game
 py.display.set_caption("Breakout")
 
@@ -21,19 +22,56 @@ py.display.flip()
 #creates ball image; having problems doing inside a class
 ball = py.image.load("C:/python/ballNoBackground.png")
 ballRect = ball.get_rect()
-ballRect.x = 400
-ballRect.y = 300
+
+#randomizes initial location with assumed paddle location under 100px and assumed brick location over 600px
+ballRect.x = random.randrange(20, 680)
+ballRect.y = random.randrange(100, 600)
+
+#variable to change ball direction
+ballDirection = [-1, -1]
 #code for the game
 while 1:
+
     #how to react to events like key presses or mouse movement
     for event in py.event.get():
+
         #exits game if you press close window
         if event.type == py.QUIT:
             sys.exit()
         #add other reactions to user here as elif
+
+    #check if ball is below paddle 
+    if ballRect.bottom <= 0:
+        pass #do something that means you lost
+
+    #check if ball is at top
+    if ballRect.top >= 100:
+        pass #do something that means you won/move on
+
+    #if ball hits walls, send in opposite direction [needs fine tuning for angles, not opposite]
+    if ballRect.left == 0 or ballRect.right == 800:
+        ballDirection[0] = -ballDirection[0]
+        ballDirection[1] = -ballDirection[1]
+
+
     
-    #adds ball 
-    gameDisplay.blit(ball, (ballRect.x, ballRect.y))
+        
+    #slows down game
+    py.time.delay(10)
+
+
+
+    #paints over last screen
+    gameDisplay.fill((0,0,0))
+
+    #moves ball [so far in only one direction]
+    ballRect = ballRect.move(ballDirection)
+
+    #adds ball to screen 
+    gameDisplay.blit(ball, ballRect)
+
+    #renders screen
     py.display.flip()
+
 #exits python
 quit()
