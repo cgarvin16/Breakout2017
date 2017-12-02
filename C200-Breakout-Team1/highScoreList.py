3892378cd891e23d383eeec4cfa4e85bbd669028
Charles Brooks <charlesbrooks@149-161-131-234.dhcp-bl.indiana.edu>
2017-12-02 17:02:07 -0500

    logan told me to commit

--- C200-Breakout-Team1/highScoreList.py
+++ C200-Breakout-Team1/highScoreList.py
@@ -1,7 +1,5 @@
-open("highScores.txt")
-
 variable = open("highScores.txt", "a")
-name = input("Add 3 initials")
+name = input("Add 3 initials: ")
 score = 0
-variable.write("{0}_ _ _ _{1}".format(name, score))
+variable.write("{0}. . . .{1}\n".format(name, score))
 variable.close()
