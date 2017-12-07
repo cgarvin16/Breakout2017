24d93e7b6826c30eb8f6949d89717d0d4ffe4910
Carolyn Garvin <ctgarvin@iu.edu>
2017-12-07 15:46:05 -0500

    progress

--- C200-Breakout-Team1/hs_screen.py
+++ C200-Breakout-Team1/hs_screen.py
@@ -1,4 +1,11 @@
-#score calculation testing file
+import pygame as py
+import sys
+
+py.init()
+py.font.init()
+py.init()
+
+gameDisplay = py.display.set_mode((800, 700))
 
 def addScore1(score):
     #opens static file to read scores and check against current score
@@ -77,7 +84,7 @@ def topTenScores():
     print(noLineScoreList2)
     print(readyScoreList2)
     print(sortedList2)
-    print(finalHighScores)
+    print("final high scores: {0}".format(finalHighScores))
 
     #closes file
     highScores2.close()
@@ -96,7 +103,70 @@ def overWriteScores():
     #closes highScores.txt
     highScores.close()
 
+def hs_screen():
+
+    print("final high score after functions: {0}".format(finalHighScores))
+    white = (255,255,255)
+    black = (0,0,0)
+    red = (255,0,0)
+    green = (0,200,0)
+    blue = (0,0,255)
+
+    gameDisplay.fill(black)
+
+    largeText = py.font.SysFont("times", 100)
+    text = py.font.Font.render(largeText, "Higscores:", 0, white, None)
+    gameDisplay.blit(text, (150, 0))
+
+    instr = py.font.SysFont("calibri",30)
+    instrText = py.font.Font.render(instr,"Welcome to Winter Breakout!", 0, red, None)
+    gameDisplay.blit(instrText,(230,200))
+
+    instr = py.font.SysFont("calibri",20)
+    instrText = py.font.Font.render(instr,"To play this game, you need to move your paddle with the left and right arrow keys.", 0, blue, None)
+    gameDisplay.blit(instrText,(80,300))
+
+    instr = py.font.SysFont("calibri",20)
+    instrText = py.font.Font.render(instr,"You must knock the ball with your paddle and try to reach the top!", 0, blue, None)
+    gameDisplay.blit(instrText,(80,320))
+
+    instr = py.font.SysFont("calibri",20)
+    instrText = py.font.Font.render(instr,"The ball must hit each brick at least one time to break through.", 0, blue, None)
+    gameDisplay.blit(instrText,(80,340))
+
+    instr = py.font.SysFont("calibri",20)
+    instrText = py.font.Font.render(instr,"*Once you reach level 2 and above the bricks may require more hits to break through.*", 0, blue, None)
+    gameDisplay.blit(instrText,(80,360))
+
+    instr = py.font.SysFont("calibri",20)
+    instrText = py.font.Font.render(instr,"1: {0} -- {1}".format(finalHighScores[0][0],finalHighScores[0][1]), 0, blue, None)
+    gameDisplay.blit(instrText,(80,380))
+
+    instr = py.font.SysFont("calibri",20)
+    instrText = py.font.Font.render(instr,"you will pass the level and move on! Be careful! don't miss the ball or you will lose a life!", 0, blue, None)
+    gameDisplay.blit(instrText,(80,400))
+
+    instr = py.font.SysFont("calibri",20)
+    instrText = py.font.Font.render(instr,"Now that you know how to play, have fun and break down those bricks!", 0, blue, None)
+    gameDisplay.blit(instrText,(80,420))
+
+    py.display.update()
+    clock = py.time.Clock()
+    clock.tick(15)
+
 finalHighScores = []
-addScore1(100)
+print(finalHighScores)
+addScore1(200)
 topTenScores()
 overWriteScores()
+while 1:        
+    paused = True
+    for e in py.event.get():
+        if py.key.get_pressed()[py.K_p]:
+            paused == True
+        elif e.type == py.QUIT:
+            sys.exit()
+        elif py.key.get_pressed()[py.K_ESCAPE]:
+            sys.exit()
+    hs_screen()
+
