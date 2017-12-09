44b0b430c9fd005be6800ca5930f6453c6ff715e
Charles Brooks <charlesbrooks@149-161-143-122.dhcp-bl.indiana.edu>
2017-12-09 18:07:32 -0500

    added sounds and created a file for the sound effects

---
+++ C200-Breakout-Team1/soundEffect.py
@@ -0,0 +1,14 @@
+#When ball hit 
+effect = pygame.mixer.Sound('bad_disk_x.wav')
+effect.play()
+
+#When ball hits brick
+effect = pygame.mixer.Sound('boing2.wav')
+effect.play()
+
+effect = pygame.mixer.Sound('glass_shatter_c.wav')
+effect.play()
+
+
+effect = pygame.mixer.Sound('hamer_anvil3.wav')
+effect.play()
